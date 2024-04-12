70dae0c14d565b82d1f92bf7b1855a601a8fa1ab
Joon Lee <joon.lee727@gmail.com>
2024-04-12 16:00:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/murexport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/murexport/actions/workflows/bump.yml)
+
+# murex
+
+A smarter shell and scripting environment with advanced features designed for usability, safety and productivity (eg smarter DevOps tooling)
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/murexport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/murexport.git'), string(name: 'PORT_DESCRIPTION', value: 'A smarter shell and scripting environment with advanced features designed for usability, safety and productivity (eg smarter DevOps tooling)' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
