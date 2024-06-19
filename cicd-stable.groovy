951904c4a59738b772ba5f52cd04a46e9fae1772
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2024-06-19 14:56:18 -0400

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/murexport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/murexport.git'), string(name: 'PORT_DESCRIPTION', value: 'A smarter shell and scripting environment with advanced features designed for usability, safety and productivity (eg smarter DevOps tooling)' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1"), string(name: 'NODE_LABEL', value: "go_122") ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/murexport.git'), string(name: 'PORT_DESCRIPTION', value: 'A smarter shell and scripting environment with advanced features designed for usability, safety and productivity (eg smarter DevOps tooling)' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1"), string(name: 'NODE_LABEL', value: "v3r1") ]
   }
 }
